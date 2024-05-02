f5ae85f050e1cfcc60b509a47f30113eae01b198
Conrad Ludgate <conradludgate@gmail.com>
2024-05-02 17:11:35 +0100

    support other link types

--- src/lib.rs
+++ src/lib.rs
@@ -151,6 +151,121 @@ where
     }
 }
 
+unsafe impl<L: DefaultLinkOps, D: ?Sized>
+    intrusive_collections::singly_linked_list::SinglyLinkedListOps for LinkedListDynLinkOps<L, D>
+where
+    L::Ops: intrusive_collections::LinkOps<LinkPtr = NonNull<L>>
+        + intrusive_collections::singly_linked_list::SinglyLinkedListOps,
+{
+    #[inline]
+    unsafe fn next(&self, ptr: Self::LinkPtr) -> Option<Self::LinkPtr> {
+        self.ops
+            .next(LinkedListDynLink::to_link(ptr))
+            .map(LinkedListDynLink::from_link)
+    }
+
+    #[inline]
+    unsafe fn set_next(&mut self, ptr: Self::LinkPtr, next: Option<Self::LinkPtr>) {
+        self.ops.set_next(
+            LinkedListDynLink::to_link(ptr),
+            next.map(LinkedListDynLink::to_link),
+        )
+    }
+}
+
+unsafe impl<L: DefaultLinkOps, D: ?Sized> intrusive_collections::linked_list::LinkedListOps
+    for LinkedListDynLinkOps<L, D>
+where
+    L::Ops: intrusive_collections::LinkOps<LinkPtr = NonNull<L>>
+        + intrusive_collections::linked_list::LinkedListOps,
+{
+    #[inline]
+    unsafe fn next(&self, ptr: Self::LinkPtr) -> Option<Self::LinkPtr> {
+        self.ops
+            .next(LinkedListDynLink::to_link(ptr))
+            .map(LinkedListDynLink::from_link)
+    }
+
+    #[inline]
+    unsafe fn set_next(&mut self, ptr: Self::LinkPtr, next: Option<Self::LinkPtr>) {
+        self.ops.set_next(
+            LinkedListDynLink::to_link(ptr),
+            next.map(LinkedListDynLink::to_link),
+        )
+    }
+
+    unsafe fn prev(&self, ptr: Self::LinkPtr) -> Option<Self::LinkPtr> {
+        self.ops
+            .prev(LinkedListDynLink::to_link(ptr))
+            .map(LinkedListDynLink::from_link)
+    }
+
+    unsafe fn set_prev(&mut self, ptr: Self::LinkPtr, prev: Option<Self::LinkPtr>) {
+        self.ops.set_prev(
+            LinkedListDynLink::to_link(ptr),
+            prev.map(LinkedListDynLink::to_link),
+        )
+    }
+}
+
+unsafe impl<L: DefaultLinkOps, D: ?Sized> intrusive_collections::rbtree::RBTreeOps
+    for LinkedListDynLinkOps<L, D>
+where
+    L::Ops: intrusive_collections::LinkOps<LinkPtr = NonNull<L>>
+        + intrusive_collections::rbtree::RBTreeOps,
+{
+    unsafe fn left(&self, ptr: Self::LinkPtr) -> Option<Self::LinkPtr> {
+        self.ops
+            .left(LinkedListDynLink::to_link(ptr))
+            .map(LinkedListDynLink::from_link)
+    }
+
+    unsafe fn right(&self, ptr: Self::LinkPtr) -> Option<Self::LinkPtr> {
+        self.ops
+            .right(LinkedListDynLink::to_link(ptr))
+            .map(LinkedListDynLink::from_link)
+    }
+
+    unsafe fn parent(&self, ptr: Self::LinkPtr) -> Option<Self::LinkPtr> {
+        self.ops
+            .parent(LinkedListDynLink::to_link(ptr))
+            .map(LinkedListDynLink::from_link)
+    }
+
+    unsafe fn color(&self, ptr: Self::LinkPtr) -> intrusive_collections::rbtree::Color {
+        self.ops.color(LinkedListDynLink::to_link(ptr))
+    }
+
+    unsafe fn set_left(&mut self, ptr: Self::LinkPtr, left: Option<Self::LinkPtr>) {
+        self.ops.set_left(
+            LinkedListDynLink::to_link(ptr),
+            left.map(LinkedListDynLink::to_link),
+        )
+    }
+
+    unsafe fn set_right(&mut self, ptr: Self::LinkPtr, right: Option<Self::LinkPtr>) {
+        self.ops.set_right(
+            LinkedListDynLink::to_link(ptr),
+            right.map(LinkedListDynLink::to_link),
+        )
+    }
+
+    unsafe fn set_parent(&mut self, ptr: Self::LinkPtr, parent: Option<Self::LinkPtr>) {
+        self.ops.set_parent(
+            LinkedListDynLink::to_link(ptr),
+            parent.map(LinkedListDynLink::to_link),
+        )
+    }
+
+    unsafe fn set_color(
+        &mut self,
+        ptr: Self::LinkPtr,
+        color: intrusive_collections::rbtree::Color,
+    ) {
+        self.ops.set_color(LinkedListDynLink::to_link(ptr), color)
+    }
+}
+
 pub unsafe trait DynAdaptor<L, D: ?Sized> {
     /// Gets a reference to the link for the given object.
     ///
